93d263dc8a53b7df2d2cfb44e9ec85d3d902d2df
CWD <cwd@cloudspokes.com>
2013-09-18 18:34:51 -0400

    fixing config the right way

--- config.js
+++ config.js
@@ -1,15 +1,16 @@
 exports.PORT = process.env.PORT || 5000; // use heroku's dynamic port or 5000 if localhost
+exports.HOST=process.env.HOST || 'localhost';
 exports.DEBUG = true;
 exports.ENVIRONMENT = (process.env.state==='production')?'production':'sandbox';
-exports.CALLBACK_URL = 'http://localhost:' + this.PORT + '/oauth/callback';
+exports.CALLBACK_URL = this.HOST+'-:' + this.PORT + '/oauth/callback';
 exports.PUSH_TOPIC = 'CDFActivityUpdates';
 
-exports.CLIENT_ID = '';
-exports.CLIENT_SECRET = '';
+exports.CLIENT_ID = process.env.CLIENT_SECRET;
+exports.CLIENT_SECRET = process.env.CLIENT_SECRET;
 exports.API_VERSION = 'v27.0';
 
-process.env.CDF_USERNAME='';
-process.env.CDF_PASSWORD='';
+process.env.CDF_USERNAME=process.env.CDF_USERNAME;
+process.env.CDF_PASSWORD=process.env.CDF_PASSWORD;
 
 // configuration for PRELOAD
 exports.DB = {
